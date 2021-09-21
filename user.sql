3756d097248e2b2516eb8342f5b4c0de062f3572
Karl <karlkws95@gmail.com>
2021-09-21 10:32:37 +0800

    first sql file

---
+++ user.sql
@@ -0,0 +1,114 @@
+use b13_group1;
+
+DROP TABLE IF EXISTS `user`;
+
+create table user  (
+	user_id INT AUTO_INCREMENT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	mobile VARCHAR(50),
+	password VARCHAR(50),
+	PRIMARY KEY (user_id)
+);
+
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (1, 'Holly-anne', 'Golborne', 'hgolborne0@gmpg.org', '990-960-4988', 'DsbqLh');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (2, 'Angelico', 'Arrowsmith', 'aarrowsmith1@phoca.cz', '842-385-7307', 'eiwnO1y02');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (3, 'Giovanni', 'Amsden', 'gamsden2@clickbank.net', '158-256-8113', 'nhu5PQ7hO');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (4, 'Courtney', 'Robecon', 'crobecon3@mit.edu', '482-221-6380', 'pkgxIKtkZ');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (5, 'Joyce', 'Jerwood', 'jjerwood4@bravesites.com', '384-987-5776', 'qxwT3sHm');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (6, 'Francisca', 'Rudgerd', 'frudgerd5@hostgator.com', '539-703-2685', 'D7BgtD66');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (7, 'Rheta', 'Cradoc', 'rcradoc6@wikispaces.com', '205-847-6706', '3PIGbR');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (8, 'Perri', 'Petrishchev', 'ppetrishchev7@weather.com', '591-686-2523', 'L0EMIb3o');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (9, 'Fianna', 'Shilburne', 'fshilburne8@archive.org', '155-574-1178', 'y6yddD');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (10, 'Callean', 'Losselyong', 'closselyong9@auda.org.au', '490-357-7038', 'PF68tBqs');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (11, 'Grant', 'Werny', 'gwernya@huffingtonpost.com', '973-598-1000', 'QWqZF46kH');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (12, 'Teodoor', 'Nertney', 'tnertneyb@engadget.com', '889-499-4798', 'H8s9beMyr');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (13, 'Hermione', 'Clementel', 'hclementelc@google.de', '516-176-2234', 'LsxbatBIGAi');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (14, 'Adah', 'Spedding', 'aspeddingd@wiley.com', '690-593-2819', 'G7kyIU');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (15, 'Evyn', 'Jenk', 'ejenke@t-online.de', '198-671-3635', 'C1OZdRBH5Ys');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (16, 'Magdalen', 'Blower', 'mblowerf@redcross.org', '921-576-8241', 'AEoUhYg5Iq');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (17, 'Danie', 'Kildale', 'dkildaleg@gmpg.org', '134-760-7829', '8wowHKspIcIB');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (18, 'Libbey', 'Baudin', 'lbaudinh@washington.edu', '985-296-3529', '27D0a8BQwq');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (19, 'Drucill', 'Nugent', 'dnugenti@shop-pro.jp', '484-979-4089', 'JQ9J9TKDf0xI');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (20, 'Mikaela', 'Ricardou', 'mricardouj@state.tx.us', '355-455-6539', 'Biz0H7wAqkV');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (21, 'Valentin', 'Millimoe', 'vmillimoek@yahoo.com', '376-612-3523', 'uoINyNgg7y8Y');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (22, 'Dora', 'Ferenczy', 'dferenczyl@netlog.com', '910-786-4316', 'Gn47Me');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (23, 'Mellisent', 'Bernollet', 'mbernolletm@wiley.com', '513-361-1606', 'rjHaV5kT0bE');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (24, 'Babbie', 'Damato', 'bdamaton@yahoo.com', '607-479-9026', 'wtt8k3vo');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (25, 'Edita', 'Wilbore', 'ewilboreo@hexun.com', '480-967-3843', 'drf9LyLftV1');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (26, 'Jeanine', 'Feaviour', 'jfeaviourp@msu.edu', '513-631-8115', 'vZwRRuznzP3L');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (27, 'Juana', 'Bernhard', 'jbernhardq@unicef.org', '648-348-3890', 'GbmikfR');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (28, 'Jaquith', 'DeSousa', 'jdesousar@yahoo.co.jp', '932-815-9505', '03onkEvmrJ');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (29, 'Sherwynd', 'Ariss', 'sarisss@theatlantic.com', '542-498-6267', 'JV0CKwbY5mr');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (30, 'Erastus', 'Hannaway', 'ehannawayt@sphinn.com', '379-512-1624', 'ejDNDWyuH');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (31, 'Lucien', 'Skea', 'lskeau@scientificamerican.com', '527-294-1110', 'rw3WwwsiH');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (32, 'Tony', 'Bryan', 'tbryanv@dell.com', '895-920-4135', 'X6ykqzdX8C');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (33, 'Denis', 'Chadband', 'dchadbandw@tamu.edu', '277-494-5912', 'yOxUzPBbVgCe');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (34, 'Dani', 'Cordero', 'dcorderox@parallels.com', '864-144-4777', 'EWylw9');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (35, 'Ferne', 'MacDowall', 'fmacdowally@nasa.gov', '609-461-2942', 'FoC8Qpd');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (36, 'Corena', 'Dotson', 'cdotsonz@bbc.co.uk', '741-548-1271', 'VrkENU');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (37, 'Adrianna', 'Farans', 'afarans10@ustream.tv', '444-319-1936', '3psWesDzAyy');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (38, 'Katy', 'Stoltz', 'kstoltz11@nps.gov', '129-894-7692', 'nBjZorpX');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (39, 'Clerc', 'Looney', 'clooney12@aboutads.info', '540-770-7297', 'ST0tWTTo');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (40, 'Abner', 'Eckels', 'aeckels13@slideshare.net', '570-169-3637', 'C79alKpE');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (41, 'Erinn', 'Bazire', 'ebazire14@webeden.co.uk', '120-868-2751', 'u9eG1DulV');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (42, 'Giorgia', 'Moline', 'gmoline15@51.la', '360-645-5384', '00vWhLOxyMhg');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (43, 'Gardie', 'O''Donegan', 'godonegan16@paypal.com', '698-149-4183', '4B4g1nt6x');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (44, 'Rafferty', 'Hendrich', 'rhendrich17@clickbank.net', '993-174-8084', 'jclmTmZ');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (45, 'Dominica', 'Haldin', 'dhaldin18@hud.gov', '178-693-8835', 'nOIdIhnFw');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (46, 'Lynett', 'Guerrieri', 'lguerrieri19@umich.edu', '186-702-9258', 'jPE7pAn');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (47, 'Noland', 'Seamen', 'nseamen1a@purevolume.com', '399-573-6502', 'U0JRdP0nx');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (48, 'Franklyn', 'Bursnoll', 'fbursnoll1b@cmu.edu', '337-309-5221', 'a2GEIk7');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (49, 'Murdock', 'Herkess', 'mherkess1c@bigcartel.com', '463-657-8958', 'ZOkgbMLO2uOh');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (50, 'Leonora', 'Honsch', 'lhonsch1d@networkadvertising.org', '844-876-1458', '5PsXfskkrJ');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (51, 'Marylou', 'Lepere', 'mlepere1e@mozilla.org', '812-609-7304', 'u0Y4pApIM');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (52, 'Tobye', 'Burbury', 'tburbury1f@europa.eu', '665-327-4499', 'z4cl3iY');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (53, 'Bryana', 'Glazyer', 'bglazyer1g@dot.gov', '182-754-9939', '3GMtXUN');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (54, 'Grange', 'Plummer', 'gplummer1h@adobe.com', '767-866-5858', 'hAIMopeyl4yD');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (55, 'Cheslie', 'Rawlence', 'crawlence1i@123-reg.co.uk', '680-608-6256', 'VaHVaC');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (56, 'Byrle', 'Jobey', 'bjobey1j@economist.com', '418-637-2346', 'wAJI5pmhkQJP');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (57, 'Hoyt', 'Ladyman', 'hladyman1k@typepad.com', '327-147-9876', 'ut2rqkC1W');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (58, 'Abbot', 'Hallick', 'ahallick1l@studiopress.com', '941-261-4451', 'dv4xTEHQSI5V');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (59, 'Quentin', 'Sewter', 'qsewter1m@themeforest.net', '395-777-6068', 'hv1vK0kCWd');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (60, 'Ashely', 'Hounson', 'ahounson1n@cafepress.com', '754-869-2763', 'znJ92kJlsN');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (61, 'Estele', 'Foux', 'efoux1o@creativecommons.org', '101-925-6341', 'mPqp2QnKN');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (62, 'Pembroke', 'Fery', 'pfery1p@adobe.com', '447-205-5505', '4DlHjF');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (63, 'Stacee', 'Offill', 'soffill1q@sogou.com', '705-466-3240', 'yia8mMBeG');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (64, 'Neill', 'Gouny', 'ngouny1r@foxnews.com', '124-764-6743', 'zixMllg');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (65, 'Melissa', 'Hallums', 'mhallums1s@vk.com', '203-345-1190', 'LyIyas3xw');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (66, 'Pen', 'Bradd', 'pbradd1t@goodreads.com', '932-608-0788', 'OPVI3lMkws');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (67, 'Celene', 'Celiz', 'cceliz1u@theguardian.com', '981-696-0019', 'uh0sqn');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (68, 'Kellie', 'Janaud', 'kjanaud1v@bandcamp.com', '781-968-8585', 'GOXKVZbPh');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (69, 'Kristien', 'Leckenby', 'kleckenby1w@sina.com.cn', '316-191-0276', 'i4H94RYtQ');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (70, 'Cam', 'Gladwin', 'cgladwin1x@shareasale.com', '681-522-2093', 'pI7u4w9TH');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (71, 'Cahra', 'Boncore', 'cboncore1y@list-manage.com', '981-249-3342', 'qMnBtJBH8u');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (72, 'Bertie', 'Gingold', 'bgingold1z@squidoo.com', '598-524-6506', 'Pz5lqp');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (73, 'Lonnie', 'Cropp', 'lcropp20@indiegogo.com', '925-388-2023', 'hS6v39D0Pw6');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (74, 'Lauri', 'Scatchar', 'lscatchar21@ihg.com', '815-825-9078', 'SAXbal8jc9');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (75, 'Violante', 'Purtell', 'vpurtell22@nsw.gov.au', '504-189-9976', 'Nk9BbR7M');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (76, 'Harcourt', 'Walework', 'hwalework23@wordpress.com', '790-654-4645', 'xGZDVW');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (77, 'Bone', 'Snoxall', 'bsnoxall24@zimbio.com', '120-230-9510', 'NyGNeuR5R79');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (78, 'Frants', 'Fountaine', 'ffountaine25@pcworld.com', '500-529-5598', '4FpqOqaSV');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (79, 'Reggie', 'Spensley', 'rspensley26@answers.com', '490-386-3718', 'HTpWDLnAv');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (80, 'Remus', 'Eggins', 'reggins27@multiply.com', '395-226-9636', 'zLLVhMBTs');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (81, 'Korey', 'Geater', 'kgeater28@issuu.com', '471-579-3135', 'sDxmmUS9CM');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (82, 'Kirk', 'Milmo', 'kmilmo29@google.com', '323-392-8320', 'rRUpPKFdjNar');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (83, 'Morty', 'Halesworth', 'mhalesworth2a@netvibes.com', '398-286-8649', 'O9MDqYp');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (84, 'Karia', 'Lademann', 'klademann2b@craigslist.org', '334-375-9112', 'YFSnUX');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (85, 'Giffie', 'Gowanlock', 'ggowanlock2c@yandex.ru', '399-806-4607', 'Dg4mcidn');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (86, 'Allys', 'Astie', 'aastie2d@ehow.com', '231-411-4840', 'WLLa1V');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (87, 'Virge', 'Gauch', 'vgauch2e@unesco.org', '145-950-7567', '8YAAtWfAS');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (88, 'Arda', 'Willoughey', 'awilloughey2f@amazon.com', '246-345-9454', 'zHehp7MNqxA');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (89, 'Archibold', 'Schwerin', 'aschwerin2g@imageshack.us', '552-935-2843', '8xcwIoG');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (90, 'Bond', 'McCarry', 'bmccarry2h@aol.com', '361-372-3714', 'Qcd8JF4');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (91, 'Sergent', 'Godding', 'sgodding2i@discovery.com', '763-949-4795', 'nSp4pErsr');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (92, 'Shelley', 'Emma', 'semma2j@apple.com', '198-876-5213', 'JrUBC2HPKq');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (93, 'Kerwin', 'Wolverson', 'kwolverson2k@slate.com', '327-229-6970', 'Qx9V04yRB');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (94, 'Gabriele', 'Castanyer', 'gcastanyer2l@posterous.com', '651-832-0818', 'hvwhbH8LBn3r');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (95, 'Demott', 'McGrayle', 'dmcgrayle2m@shareasale.com', '493-618-9197', 'Jw8Crz');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (96, 'Leland', 'Enterle', 'lenterle2n@joomla.org', '277-486-5092', 'zOE37pGpa6yk');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (97, 'Sayre', 'Augur', 'saugur2o@answers.com', '466-935-6028', 'XL7YPoy');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (98, 'Addie', 'Gunningham', 'agunningham2p@example.com', '412-609-5559', 'cG2jkcD');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (99, 'Moises', 'Kanwell', 'mkanwell2q@weibo.com', '823-915-9278', 'omVhff');
+insert into user  (user_id, first_name, last_name, email, mobile, password) values (100, 'Danette', 'Hamon', 'dhamon2r@spiegel.de', '211-662-1005', 'UgvJOj6BJMpA');
