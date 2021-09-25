2b25ae67750fe9f8193d3d40dda5b8616bf9bdb1
AndrewLim <andrewlimyh@gmail.com>
2021-09-25 13:02:04 +0800

    add balance and link to hometesting page

--- front-end/hometesting.js
+++ front-end/hometesting.js
@@ -3,8 +3,28 @@ function getEmail(){
     var querryStr = "http://localhost:3000/admin";
     console.log(querryStr);
     $.getJSON(querryStr, function (response) {
-        var text = response.e_mail
-        $("#test").html(text);
+        var email = response.e_mail
+        console.log(email)
+
+
+        var emailStr =  "http://localhost:3000/user/by-email?email=" + email;
+        console.log(emailStr);
+        $.getJSON(emailStr, mydata );
+        
+
+
+          function mydata(data){
+            var text ="<ul>";
+            data.forEach(function(item){    
+            text = text +  `<li> Account:<br><br>  SGD Balance: ${item.balance_sgd}<br> USD Balance: ${item.balance_usd}<br> EUR Balance:: ${item.balance_eur}<br>  
+            GBP Balance: ${item.balance_gbp} </li>`
+          
+            });
+            text += "</ul>"
+            $(".balance").html(text);   
+        }
+
+        $("#test").html(email);
     });
   };
 
